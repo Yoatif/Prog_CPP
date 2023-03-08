2b533bdd5f5022f5463d589145dfd9d5120d3a6e
yoatif <y.retif@rns.ecolescreatives.com>
2023-03-08 08:29:41 +0100

    up

--- 2eme_cour/TictactoeCPP-main/Tictactoe.cpp
+++ 2eme_cour/TictactoeCPP-main/Tictactoe.cpp
@@ -15,45 +15,61 @@
 
     void Tictactoe::ajouteSymbole(int x, int y){
         // à compléter
-        y=x%3;
-        x=x/3;
-        _grilleDeJeu.setContent(x,y,_symboleCourant);
+        if ( _grilleDeJeu.getContent(x,y)== ' '){
+            _grilleDeJeu.setContent(x,y, _symboleCourant);
+            if (_symboleCourant== 'X'){_symboleCourant = 'O';}
+            else {_symboleCourant = 'X';}
+        }
+         else { // la case est déjà occupée (ou sort du tableau) 
+            std::cout << "Coup incorrect. Veuillez entrer un nouveau numéro de case !";
+            int numeroCase;
+            std::cin >> numeroCase;
+            this->ajouteSymbole((numeroCase - 1) % 3, (numeroCase - 1) / 3);
+        }
+            
     }
 
     bool Tictactoe::testeVictoireVerticale(){
         // à compléter
 
         while(_numeroTour !=9){
-            if((_grilleDeJeu.getContent(0,0)==_grilleDeJeu.getContent(0,1)&&_grilleDeJeu.getContent(0,1)==_grilleDeJeu.getContent(0,2))||(_grilleDeJeu.getContent(1,0)==_grilleDeJeu.getContent(1,1)&&_grilleDeJeu.getContent(1,1)==_grilleDeJeu.getContent(1,2))||(_grilleDeJeu.getContent(2,0)==_grilleDeJeu.getContent(2,1)&&_grilleDeJeu.getContent(2,1)==_grilleDeJeu.getContent(2,2))){
-                std:: cout << "gg0";
-                return true;
+            for (int i = 0; i < 2; i++){
+                if((_grilleDeJeu.getContent(i,0)==_grilleDeJeu.getContent(i,1))&&(_grilleDeJeu.getContent(i,1)==_grilleDeJeu.getContent(i,2))){
+                    std:: cout << "gg0";
+                    return true;
+                }
+                else{
+                    return false;
+                }
             }
-            else{
-                return false;
-            }
-            
         }
+        return false;
 
     }
 
     bool Tictactoe::testeVictoireHorizontale(){
         // à compléter 
-        while(_numeroTour != 9 ){
-            if((_grilleDeJeu.getContent(0,0)==_grilleDeJeu.getContent(1,0)&&_grilleDeJeu.getContent(1,0)==_grilleDeJeu.getContent(2,0))||(_grilleDeJeu.getContent(0,2)==_grilleDeJeu.getContent(1,2)&&_grilleDeJeu.getContent(1,2)==_grilleDeJeu.getContent(2,2))||(_grilleDeJeu.getContent(0,1)==_grilleDeJeu.getContent(1,1)&&_grilleDeJeu.getContent(1,1)==_grilleDeJeu.getContent(2,1))){
-                std:: cout << "gg1";
-                return true;
+        while(_numeroTour !=9){
+            for (int i = 0; i < 2; i++){
+                if((_grilleDeJeu.getContent(0,i)==_grilleDeJeu.getContent(1,i))&&(_grilleDeJeu.getContent(1,i)==_grilleDeJeu.getContent(2,i))){
+                    std:: cout << "gg1";
+                    return true;
+                }
+                else{
+                    return false;
+                }
             }
-            else{
-                return false;
-            }       
         }
+        return false;
+
     }
 
     bool Tictactoe::testeVictoireDiagonale(){
         
         // à compléter
         while(_numeroTour!=9){
-            if((_grilleDeJeu.getContent(0,0)==_grilleDeJeu.getContent(1,1)&&_grilleDeJeu.getContent(1,1)==_grilleDeJeu.getContent(2,2))||(_grilleDeJeu.getContent(2,0)==_grilleDeJeu.getContent(1,1)&&_grilleDeJeu.getContent(1,1)==_grilleDeJeu.getContent(0,2))){
+            if(((_grilleDeJeu.getContent(0,0)==_symboleCourant)&&(_grilleDeJeu.getContent(1,1)==_symboleCourant)&&(_grilleDeJeu.getContent(2,2)==_symboleCourant))||
+            ((_grilleDeJeu.getContent(0,2)==_symboleCourant)&&(_grilleDeJeu.getContent(1,1)==_symboleCourant)&&(_grilleDeJeu.getContent(2,0)==_symboleCourant))){
                 std:: cout << "gg2";
                 return true;
             }
--- 2eme_cour/TictactoeCPP-main/main.cpp
+++ 2eme_cour/TictactoeCPP-main/main.cpp
@@ -16,8 +16,9 @@ int main(){
     while(!partiegagnee && !(jeu->testeJeuNul())){
         
         jeu->afficheGrille();
+        std::cout << "veuillez entrer un numéro de case [1-9] !";
         std::cin >> numeroCase;
-        jeu->ajouteSymbole(numeroCase,0);
+        jeu->ajouteSymbole(((numeroCase -1)%3),(numeroCase -1)/3);
         partiegagnee = jeu->testeVictoireDiagonale() || jeu->testeVictoireHorizontale() || jeu->testeVictoireVerticale();
         jeu->finTour();
     }
